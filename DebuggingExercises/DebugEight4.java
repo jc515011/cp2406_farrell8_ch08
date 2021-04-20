74e3c3581d5ccee4a1718944bba6fa62531bf34e
jc515011 <yating.lei@my.jcu.edu.au>
2021-04-19 21:20:55 -0700

    Prac 5 Task 3 My solution

--- DebuggingExercises/DebugEight4.java
+++ DebuggingExercises/DebugEight4.java
@@ -1,28 +1,23 @@
 // Allows user to enter a series of words
 // and displays them in reverse order
 import javax.swing.*;
-public class DebugEight4
+public class FixDebugEight4
 {
    public static void main(String[] args)
    {
-      int x = 0, y;
+      int x=0, y;
       String array[] = new String[100];
       String entry;
       final String STOP = "XXX";
-      StringBuffer message = new
-          StringBuffer("The words in reverse order are\n");
-     
-      entry = JOptionPane.showInputDialog(null,
-        "Enter any word\n" +
-        "Enter " + STOP " when you want to stop"); 
-      while(!(entry.equals(STOP))
+      StringBuffer message = new StringBuffer("The words in reverse order are\n");
+
+      entry = JOptionPane.showInputDialog(null, "Enter any word\n" + "Enter " + STOP + " when you want to stop");
+      while(!entry.equals(STOP))
       {
-         array[STOP] = entry;
-         entry = JOptionPane.showinputDialog(null,
-            "Enter another word\n" +
-            "Enter " + STOP + " when you want to stop"); 
+         array[x++] = entry;
+         entry = JOptionPane.showInputDialog(null, "Enter another word\n" + "Enter " + STOP + " when you want to stop");
       }
-      for(y = 0; y > 0; ++y);
+      for(y= (x-1); y > -1; y--)
       {
          message.append(array[y]);
          message.append("\n");
